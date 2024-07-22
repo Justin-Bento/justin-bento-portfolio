caeebc41a8972b0bba50584e8fc9ca5ffe50ed69
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-22 18:03:50 -0400

    Transferred styles form shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
       <main className="">
         <section className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="min-h-dvh grid place-content-center">
-            <h1>Hello World, welcome to my homepage.</h1>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              The Joke Tax Chronicles
+            </h1>
+            <p className="text-xl leading-7 [&:not(:first-child)]:mt-3">
+              Once upon a time, in a far-off land, there was a very lazy king who
+              spent all day lounging on his throne. One day, his advisors came to him
+              with a problem: the kingdom was running out of money.
+            </p>
           </div>
         </section>
       </main>
