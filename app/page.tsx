6c22b45956d0f23ed58a40a67672ea4402da82a4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-22 17:40:46 -0400

    Changed div layout for mobile: Full-width with padded constraint.
    - Replaced centered div with full-width layout on mobile devices.
    
    - Added padding to the section element to constrain content.
    
    - Ensured content within the div has a max width equal to the parent element.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <>
       <main className="">
-        <section className="min-h-dvh grid place-content-center">
-          <h1>Hello World, welcome to my homepage.</h1>
+        <section className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="min-h-dvh grid place-content-center">
+            <h1>Hello World, welcome to my homepage.</h1>
+          </div>
         </section>
       </main>
     </>
