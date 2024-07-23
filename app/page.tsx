c19db606e00c22971cacea0fd76f3e0d64e2ad2a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-22 23:08:58 -0400

    Formatted document with prettier default settings ensuring code consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -11,9 +11,8 @@ export default function Home() {
                 From Designs to Dynamic Websites: Join Me On My Coding Adventure!
               </h1>
               <p className="text-xl leading-7 [&:not(:first-child)]:mt-3">
-                Once upon a time, in a far-off land, there was a very lazy king who
-                spent all day lounging on his throne. One day, his advisors came to him
-                with a problem: the kingdom was running out of money.
+                Once upon a time, in a far-off land, there was a very lazy king who spent all day lounging on his throne. One day, his
+                advisors came to him with a problem: the kingdom was running out of money.
               </p>
               <div className="flex items-center flex-col lg:flex-row gap-4 mt-8">
                 <Button>Explore My Portfolio</Button>
@@ -24,5 +23,5 @@ export default function Home() {
         </section>
       </main>
     </>
-  )
+  );
 }
