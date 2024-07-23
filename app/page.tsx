0a3b8b302b6b9c0d82dccf6d8ef395fa739c6747
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-22 23:15:42 -0400

    Changed the wrapper around HomeContent to an <article> for better semantics.

--- app/page.tsx
+++ app/page.tsx
@@ -16,20 +16,18 @@ export default function Home() {
 
 export function HomeContent() {
   return (
-    <>
-      <div className="max-w-4xl mx-auto">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          From Designs to Dynamic Websites: Join Me On My Coding Adventure!
-        </h1>
-        <p className="text-xl leading-7 [&:not(:first-child)]:mt-3">
-          Once upon a time, in a far-off land, there was a very lazy king who spent all day lounging on his throne. One day, his advisors
-          came to him with a problem: the kingdom was running out of money.
-        </p>
-        <div className="flex items-center flex-col lg:flex-row gap-4 mt-8">
-          <Button>Explore My Portfolio</Button>
-          <Button variant="outline">Let's Connect</Button>
-        </div>
+    <article className="max-w-4xl mx-auto">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        From Designs to Dynamic Websites: Join Me On My Coding Adventure!
+      </h1>
+      <p className="text-xl leading-7 [&:not(:first-child)]:mt-3">
+        Once upon a time, in a far-off land, there was a very lazy king who spent all day lounging on his throne. One day, his advisors came
+        to him with a problem: the kingdom was running out of money.
+      </p>
+      <div className="flex items-center flex-col lg:flex-row gap-4 mt-8">
+        <Button>Explore My Portfolio</Button>
+        <Button variant="outline">Let's Connect</Button>
       </div>
-    </>
+    </article>
   );
 }
