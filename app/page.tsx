299a4d1f28555a9319b054911d62c6d924468b16
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-23 21:02:11 -0400

    Copied content from the existing website to <p> element.

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,9 @@ export function HomeContent() {
         From Designs to Dynamic Websites: Join Me On My Coding Adventure!
       </h1>
       <p className="text-xl leading-7 [&:not(:first-child)]:mt-3">
-        Once upon a time, in a far-off land, there was a very lazy king who spent all day lounging on his throne. One day, his advisors came
-        to him with a problem: the kingdom was running out of money. The king thought long and hard, and finally came up with a plan: he would tax the kingdom.
+        I'm a Multimedia Designer venturing into the field of Front-End Development. My goal is to blend my knowledge of accessibility
+        design with the power of code to create engaging websites that everyone can enjoy. Join me on this journey of creativity and
+        interactivity!
       </p>
       <div className="flex items-center flex-col lg:flex-row lg:justify-center gap-4 mt-8">
         <Button>Explore My Portfolio</Button>
