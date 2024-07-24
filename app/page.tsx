68a329dc4348d0ed7a36a8cb87a51a4da4f3ce4e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-23 21:20:20 -0400

    Fixed responsive issues for mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -16,18 +16,18 @@ export default function Home() {
 
 export function HomeContent() {
   return (
-    <article className="max-w-4xl mx-auto lg:text-center">
+    <article className="max-w-4xl mx-auto md:text-center text-balance">
       <div className="size-20 bg-easternblue-500 rounded-full mx-auto mb-4 ring ring-neutral-800"></div>
       <p className="text-base">Hi, I'm Justin Bento.</p>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         From Designs to Dynamic Websites: Join Me On My Coding Adventure!
       </h1>
-      <p className="text-xl leading-7 [&:not(:first-child)]:mt-3 lg:text-balance">
+      <p className="text-lg lg:text-xl leading-7 [&:not(:first-child)]:mt-3 lg:text-balance">
         I'm a Multimedia Designer venturing into the field of Front-End Development. My goal is to blend my knowledge of accessibility
         design with the power of code to create engaging websites that everyone can enjoy. Join me on this journey of creativity and
         interactivity!
       </p>
-      <div className="flex items-center flex-col lg:flex-row lg:justify-center gap-4 mt-8">
+      <div className="flex items-center flex-col md:flex-row md:justify-center gap-4 mt-8">
         <Button variant="brand">Explore My Portfolio</Button>
         <Button variant="outline">Let's Connect</Button>
       </div>
