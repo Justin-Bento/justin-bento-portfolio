b1f6c79f8e65eacc322292b4fd901f3da91ceddf
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-25 18:53:06 -0400

    Added grid list with 3 columns on desktop and 1 for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function header() {
   return (
     <header>
-      <ul role="list" className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <ul role="list" className="container mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 gap-4 md:grid-cols-3">
         <li>Section 1</li>
         <li>Section 2</li>
         <li>Section 3</li>
