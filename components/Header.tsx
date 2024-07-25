60f307df136a9426bcd20c6d614536d997a9de3b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-25 18:58:02 -0400

    Added some padding to give content some breathing room.

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function header() {
   return (
-    <header className="overflow-hidden bg-white shadow">
-      <ul role="list" className="container mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 gap-4 md:grid-cols-3">
+    <header className="overflow-hidden bg-white shadow py-4 sm:py-6">
+      <ul role="list" className="container mx-auto grid grid-cols-1 gap-4 md:grid-cols-3">
         <li className="flex items-start justify-between ring">Section 1</li>
         <li className="flex items-start justify-center ring">Section 2</li>
         <li className="flex items-start justify-end ring">Section 3</li>
