d01165d4878c9ec2b546d2df690ac30d62b02904
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-25 18:55:38 -0400

    Ensured items within the grid aligned justified evenly according to the container.

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ export default function header() {
   return (
     <header>
       <ul role="list" className="container mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 gap-4 md:grid-cols-3">
-        <li>Section 1</li>
-        <li>Section 2</li>
-        <li>Section 3</li>
+        <li className="flex items-start justify-between ring">Section 1</li>
+        <li className="flex items-start justify-center ring">Section 2</li>
+        <li className="flex items-start justify-end ring">Section 3</li>
       </ul>
     </header>
   );
