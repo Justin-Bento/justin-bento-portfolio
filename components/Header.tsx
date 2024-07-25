0912448a16158444b6ba1c33af7789ea6966ae7a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-25 19:11:56 -0400

    Added external link icon for Giithub

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,17 @@ export default function header() {
           </Button>
         </li>
         <li className="flex items-center justify-end">
-          <Button variant="secondary" size="sm">
+          <Button variant="secondary" size="sm" className="inline-flex items-center gap-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-black stroke-width-1 size-3"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path>
+              <path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path>
+            </svg>{' '}
             GitHub
           </Button>
         </li>
