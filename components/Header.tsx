b9fbb6b51208ed7d89da3083b8c42941ded8e950
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-25 18:58:34 -0400

    Removed ring as visual cues where no longer needed

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ export default function header() {
   return (
     <header className="overflow-hidden bg-white shadow py-4 sm:py-6">
       <ul role="list" className="container mx-auto grid grid-cols-1 gap-4 md:grid-cols-3">
-        <li className="flex items-start justify-between ring">Section 1</li>
-        <li className="flex items-start justify-center ring">Section 2</li>
-        <li className="flex items-start justify-end ring">Section 3</li>
+        <li className="flex items-start justify-between">Section 1</li>
+        <li className="flex items-start justify-center">Section 2</li>
+        <li className="flex items-start justify-end">Section 3</li>
       </ul>
     </header>
   );
