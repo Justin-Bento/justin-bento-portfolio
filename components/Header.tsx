4a8f11a51df85b06043559282dba2bd56c73f41e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-25 18:57:05 -0400

    Ensured content didn't overflow outside of the container

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function header() {
   return (
-    <header>
+    <header className="overflow-hidden bg-white shadow">
       <ul role="list" className="container mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 gap-4 md:grid-cols-3">
         <li className="flex items-start justify-between ring">Section 1</li>
         <li className="flex items-start justify-center ring">Section 2</li>
