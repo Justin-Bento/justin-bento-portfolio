68ac4e9a6d10fdadaef32341734eee6183df6011
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-25 18:35:11 -0400

    imported component to pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button';
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <>
+      <Header />
       <main className="">
         <section className="mx-auto max-w-7xl p-4 sm:px-6 lg:px-8">
           <BackgroundPattern />
@@ -24,9 +26,9 @@ export function HomeContent() {
         From Designs to Dynamic Websites: Join Me On My Coding Adventure!
       </h1>
       <p className="text-lg lg:text-xl leading-7 [&:not(:first-child)]:mt-3 lg:text-balance">
-        I'm a Multimedia Designer venturing into the field of Front-End Development. My goal is to blend my knowledge of accessibility
-        design with the power of code to create engaging websites that everyone can enjoy. Join me on this journey of creativity and
-        interactivity!
+        I'm a Multimedia Designer venturing into the field of Front-End Development. My goal is to blend my knowledge of
+        accessibility design with the power of code to create engaging websites that everyone can enjoy. Join me on this
+        journey of creativity and interactivity!
       </p>
       <div className="flex items-center flex-col md:flex-row lg:justify-center gap-4 mt-8">
         <Button variant="brand" className="w-full md:w-auto">
