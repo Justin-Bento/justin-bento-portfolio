e93d56ab5d9e9d3f3f42ab7ba694e8f9151ba5b2
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-26 11:51:13 -0400

    Added some text into the card with a max-container and some padding.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bg-easternblue-700 overflow-hidden">Footer</footer>
-  )
+    <footer className="bg-easternblue-700 overflow-hidden">
+      <div className="container mx-auto py-4 sm:py-6">
+        <p className="text-white">Some content or remove this item</p>
+      </div>
+    </footer>
+  );
 }
