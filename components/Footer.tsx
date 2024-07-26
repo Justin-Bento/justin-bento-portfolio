37f50945255a7df48d7d8ef3d92ad533756f6a89
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-26 11:54:10 -0400

    Added an unordered list to hold icons as links inside a container

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,19 @@
+import { Button } from './ui/button';
+
 export default function Footer() {
   return (
     <footer className="bg-easternblue-700 overflow-hidden">
       <div className="container mx-auto py-4 sm:py-6">
         <p className="text-white">Some content or remove this item</p>
+        <ul role="list" className="text-white">
+          <li>
+            <a href="www.example_website.com">
+              <Button variant="ghost" size="icon">
+                I1
+              </Button>
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
