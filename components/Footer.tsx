d8602e43076b3e4a97a992ee45b589f420d6c0d2
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-26 11:56:07 -0400

    Included some dummy icons for better representation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,39 @@ export default function Footer() {
     <footer className="bg-easternblue-700 overflow-hidden">
       <div className="container mx-auto py-4 sm:py-6">
         <p className="text-white">Some content or remove this item</p>
-        <ul role="list" className="text-white">
+        <ul role="list" className="flex items-center list-none text-white">
           <li>
             <a href="www.example_website.com">
               <Button variant="ghost" size="icon">
                 I1
+              </Button>
+            </a>
+          </li>
+          <li>
+            <a href="www.example_website.com">
+              <Button variant="ghost" size="icon">
+                I2
+              </Button>
+            </a>
+          </li>
+          <li>
+            <a href="www.example_website.com">
+              <Button variant="ghost" size="icon">
+                I3
+              </Button>
+            </a>
+          </li>
+          <li>
+            <a href="www.example_website.com">
+              <Button variant="ghost" size="icon">
+                I4
+              </Button>
+            </a>
+          </li>
+          <li>
+            <a href="www.example_website.com">
+              <Button variant="ghost" size="icon">
+                I5
               </Button>
             </a>
           </li>
