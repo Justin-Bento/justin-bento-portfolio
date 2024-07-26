13fb02232b08dcb249cbbd48bb9449ca43edb35c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-26 11:57:14 -0400

    Added flex to push icons to the end of the container

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { Button } from './ui/button';
 export default function Footer() {
   return (
     <footer className="bg-easternblue-700 overflow-hidden">
-      <div className="container mx-auto py-4 sm:py-6">
+      <div className="container mx-auto py-4 sm:py-6 flex flex-col md:flex-row md:items-center justify-between">
         <p className="text-white">Some content or remove this item</p>
         <ul role="list" className="flex items-center list-none text-white">
           <li>
