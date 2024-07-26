306dbace8d2e067b1ac73b28ed3d9ab7d7587f9e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-26 11:30:54 -0400

    Imported BackgroundPattern component to isolate SVG patterns from the main UI.
    By isolating the SVG patterns into a dedicated BackgroundPattern component it Reduced Complexity, Enhanced Maintainability and Improved Readability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import Header from '@/components/Header';
+import BackgroundPattern from '@/components/BackgroundPattern';
 
 export default function Home() {
   return (
