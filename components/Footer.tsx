c8f79543726e4ebb2cdc404af810d631f7469de5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-26 12:01:52 -0400

    Updated the content for the website.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-easternblue-700 overflow-hidden">
       <div className="container mx-auto py-4 sm:py-6 flex flex-col md:flex-row md:items-center justify-between">
-        <p className="text-white">Some content or remove this item</p>
+        <p className="text-white">Built and designed by Justin Bento.</p>
         <ul role="list" className="flex items-center gap-4 list-none text-white">
           <li>
             <a href="www.example_website.com">
